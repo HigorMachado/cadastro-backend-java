feff74fdb4bb7b9980b3ae20d6f33af92f48a55e
HigorMachado <higordomingosmachado@gmail.com>
2021-08-04 13:21:56 -0300

    ADD

--- src/main/java/com/projeto/java/entity/Endereco.java
+++ src/main/java/com/projeto/java/entity/Endereco.java
@@ -1,10 +1,16 @@
 package com.projeto.java.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 
@@ -16,6 +22,9 @@ public class Endereco {
     private int codigoEndereco;
 
     @Column(unique = true, length = 100, nullable = false)
-    private String nomeEndereco;
+    private String nomeEndereco; 
 
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "codigo_cliente")
+    private List<Cliente> clientes = new ArrayList<>();
 }
--- src/main/java/com/projeto/java/entity/Telefone.java
+++ src/main/java/com/projeto/java/entity/Telefone.java
@@ -1,10 +1,16 @@
 package com.projeto.java.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 
 import lombok.Data;
 
@@ -18,6 +24,7 @@ public class Telefone {
     @Column(unique = false, length = 20, nullable = false)
     private String numeroTelefone;
 
-    @Column(nullable = false)
-    private int codigoUsuario;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "codigo_cliente")
+    private List<Cliente> clientes = new ArrayList<>();
 }
